28e6a2c1e87d0025a49cbc3eff47692847d1e325
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 23:04:28 +0300

    add model concession for send client

---
+++ ServerSelectionCommittee/ConcessionSend.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    [Serializable]
+    class ConcessionSend
+    {
+        public int Id { get; set; }
+        public string NameConcession { get; set; }
+
+        public static List<ConcessionSend> GetData()
+        {
+            List<ConcessionSend> concessionSends = new List<ConcessionSend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (Concession c in db.Concessions)
+                {
+                    concessionSends.Add(
+                        new ConcessionSend()
+                        {
+                            Id = c.IdConcession,
+                            NameConcession = c.NameConcession
+                        }
+                        );
+                }
+            }
+
+            return concessionSends;
+        }
+    }
+}
