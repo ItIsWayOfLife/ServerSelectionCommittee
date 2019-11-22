198294ee83782ab736f9d93d05db38dbc9b120af
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 22:58:27 +0300

    add model department for send client

---
+++ ServerSelectionCommittee/DepartmentSend.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    [Serializable]
+    class DepartmentSend
+    {
+        public int Id { get; set; }
+        public string FullNameDepartment { get; set; }
+        public string ShortNameDepartment { get; set; }
+
+        public static List<DepartmentSend> GetData()
+        {
+            List<DepartmentSend> departmentSends = new List<DepartmentSend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (Department d in db.Departments)
+                {
+                    departmentSends.Add(
+                        new DepartmentSend()
+                        {
+                            Id = d.IdDepartment,
+                            FullNameDepartment = d.FullNameDepartment,
+                            ShortNameDepartment = d.ShortNameDepartment
+                        }
+                        );
+                }
+            }
+
+            return departmentSends;
+        }
+    }
+}
