06be764e19bb9a94eeb3e3029368dfba14df375f
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 22:33:26 +0300

    add model enrTest for send clint

---
+++ ServerSelectionCommittee/EntranceTestsSend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    class EntranceTestsSend
+    {
+        public int Id { get; set; }
+        public string NameEntranceTests { get; set; }
+
+        public static List<EntranceTestsSend> GetData()
+        {
+            List<EntranceTestsSend> entranceTestsSends = new List<EntranceTestsSend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (EntranceTests e in db.EntranceTests)
+                {
+                    entranceTestsSends.Add(
+                        new EntranceTestsSend()
+                        {
+                            Id = e.IdEntranceTests,
+                            NameEntranceTests = e.NameEntranceTests
+                        }
+                        );
+                }
+            }
+
+            return entranceTestsSends;
+        }
+    }
+}
