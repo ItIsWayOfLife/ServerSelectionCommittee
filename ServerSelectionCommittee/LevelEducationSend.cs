3b286966b0186c74ffcb6c0fc0d476e1f705721c
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 21:16:56 +0300

    add model levEd for send client

---
+++ ServerSelectionCommittee/LevelEducationSend.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    [Serializable]
+    class LevelEducationSend
+    {
+        public int Id { get; set; }
+        public string NameLevelEducation { get; set; }
+
+        public static List<LevelEducationSend> GetData()
+        {
+            List<LevelEducationSend> levelEducationSends = new List<LevelEducationSend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (LevelEducation l in db.LevelEducations)
+                {
+                    levelEducationSends.Add(
+                        new LevelEducationSend()
+                        {
+                            Id = l.IdLevelEducation,
+                            NameLevelEducation = l.NameLevelEducation
+                        }
+                        );
+                }
+            }
+
+            return levelEducationSends;
+        }
+    }
+}
