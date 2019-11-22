2a6bcf8a5ab07074eb3644d9c5b309b65ef57c51
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 21:29:59 +0300

    add model formStudy for send client

---
+++ ServerSelectionCommittee/FormStudySend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    [Serializable]
+    class FormStudySend
+    {
+        public int Id { get; set; }
+        public string NameFormStudy { get; set; }
+
+        public static List<FormStudySend> GetData()
+        {
+            List<FormStudySend> formStudySends = new List<FormStudySend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (FormStudy f in db.FormStudies)
+                {
+                    formStudySends.Add(
+                        new FormStudySend()
+                        {
+                            Id = f.IdFormStudy,
+                            NameFormStudy = f.NameFormStudy
+                        }
+                        );
+                }
+            }
+            return formStudySends;
+        }
+    }
+}
