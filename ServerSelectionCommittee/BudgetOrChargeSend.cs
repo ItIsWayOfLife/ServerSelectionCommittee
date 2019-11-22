579947cb31895e3c074a7af3a914f7720b35d054
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 23:07:17 +0300

    add model budget for send client

---
+++ ServerSelectionCommittee/BudgetOrChargeSend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    class BudgetOrChargeSend
+    {
+        public int Id { get; set; }
+        public string NameBudgetOrCharge { get; set; }
+
+        public static List<BudgetOrChargeSend> GetData()
+        {
+            List<BudgetOrChargeSend> budgetOrChargeSends = new List<BudgetOrChargeSend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (BudgetOrCharge b in db.BudgetOrCharges)
+                {
+                    budgetOrChargeSends.Add(
+                        new BudgetOrChargeSend()
+                        {
+                            Id =b.IdBudgetOrCharge,
+                            NameBudgetOrCharge = b.NameBudgetOrCharge
+                        }
+                        );
+                }
+            }
+
+            return budgetOrChargeSends;
+        }
+    }
+}
