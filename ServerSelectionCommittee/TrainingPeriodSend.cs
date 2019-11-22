53b1ccdc6453fc6c9cbaa90b1b9efee33282cfe4
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 20:33:01 +0300

    add model tp for send client

---
+++ ServerSelectionCommittee/TrainingPeriodSend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    class TrainingPeriodSend
+    {
+        public int Id { get; set; }
+        public string NameTrainingPeriod { get; set; }
+
+
+        public static List<TrainingPeriodSend> GetData()
+        {
+            List<TrainingPeriodSend> trainingPeriodSends = new List<TrainingPeriodSend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (TrainingPeriod t in db.TrainingPeriods)
+                {
+                    trainingPeriodSends.Add(
+                        new TrainingPeriodSend()
+                        {
+                            Id = t.IdTrainingPeriod,
+                            NameTrainingPeriod = t.NameTrainingPeriod
+                        }
+                        );
+                }
+            }
+            return trainingPeriodSends;
+        }
+    }
+}
