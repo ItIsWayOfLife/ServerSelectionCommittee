91d85ae62c74545859fdf30ecf512239c653ebe7
ItIsWayOfLife <antongunko1@gmail.com>
2019-11-22 21:26:11 +0300

    add model history for send client

---
+++ ServerSelectionCommittee/HistorySend.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSelectionCommittee
+{
+    [Serializable]
+    class HistorySend
+    {
+        public int Id { get; set; }
+        public int IdEnrollee { get; set; }
+        public string Operation { get; set; }
+        public DateTime CreateAt { get; set; }
+
+        public static List<HistorySend> GetData()
+        {
+            List<HistorySend> historySends = new List<HistorySend>();
+
+            using (DataContext db = new DataContext())
+            {
+                foreach (History h in db.Histories)
+                {
+                    historySends.Add(
+                        new HistorySend()
+                        {
+                            Id = h.IdHistory,
+                            IdEnrollee = h.IdEnrollee,
+                            Operation = h.Operation,
+                            CreateAt = h.CreateAt                          
+                        }
+                        );
+                }
+            }
+            return historySends;
+        }
+    }
+}
